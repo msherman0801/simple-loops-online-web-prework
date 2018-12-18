854e0d4a81f268d7fb68d3966df3c5cd93b587a2
Matthew Sherman <msherman0801@gmail.com>
2018-12-18 00:22:16 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,39 +4,39 @@ def loop_iterator(number_of_times)
   
 end
 
-def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
-  index = number_of_times
-  index.times do
-    phrase = "Welcome to Flatiron School's Web Development Course!"
-    print phrase
-  end
-end
-end
+# def times_iterator(number_of_times)
+#   # code your solution here using the "times" keyword
+#   index = number_of_times
+#   index.times do
+#     phrase = "Welcome to Flatiron School's Web Development Course!"
+#     print phrase
+#   end
+# end
+# end
 
-def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
-  counter = 0
+# def while_iterator(number_of_times)
+#   # code your solution here using the "while" keyword
+#   # hint: user a counter to tell the while loop when to stop!
+#   counter = 0
 
-  while counter <= number_of_times
-  puts "Welcome to Flatiron School's Web Development Course!"
-  counter++
-end
-end
-end
+#   while counter <= number_of_times
+#   puts "Welcome to Flatiron School's Web Development Course!"
+#   counter++
+# end
+# end
+# end
 
-def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+# def until_iterator(number_of_times)
+#   # code your solution here using the "until" keyword
+#   # hint: use a counter to tell the until loop when to stop!
+#   phrase = "Welcome to Flatiron School's Web Development Course!"
 
-end
+# end
 
-def for_iterator(number_of_times)
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
+# def for_iterator(number_of_times)
+#   # code your solution here using the "for" keyword
+#   # remember that `for` requires a range of numbers. How can we turn our number into a range?
 
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+#   phrase = "Welcome to Flatiron School's Web Development Course!"
 
-end
+# end
