b25c5503f3ffbcc0fa708f1902dff0e4f00e47fa
Matthew Sherman <msherman0801@gmail.com>
2018-12-18 00:35:03 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,7 +2,7 @@
 
 def loop_iterator(number_of_times)
   loop do
-    break if number_of_times == 8
+      break if number_of_times == 8
       puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
